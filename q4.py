524b06f914ced22618f8e0c37021eb845f4f6368
Pâmela Fernandes <98667548+pamialvs@users.noreply.github.com>
2025-02-16 21:22:04 -0300

    Add files via upload

---
+++ q4.py
@@ -0,0 +1,6 @@
+# Crie um programa que peça dois números ao usuário e exiba a soma deles.
+numero1 = int(input("Digite o primeiro número: "))
+numero2 = int(input("Digite o segundo número: "))
+resultado = numero1 + numero2
+print (resultado)
+
