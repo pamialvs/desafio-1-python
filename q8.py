bd484bbfa3bfe37f7cce3160fb71b3e96e715a44
Pâmela Fernandes <98667548+pamialvs@users.noreply.github.com>
2025-02-21 18:46:52 -0300

    Add files via upload

---
+++ q8.py
@@ -0,0 +1,4 @@
+# Crie uma tupla com os seguintes nomes: Ana, Bruno, Carla, Daniel, Eduardo e exiba o primeiro e o último nome. 
+nomes("Ana", "Bruno", "Carla", "Daniel", "Eduardo")
+print("Primeiro: ", nomes[0])
+print("Ultimo: ", nomes[4])
