1d346bb86066906eb6abd6700f5f8e70c4104269
Pâmela Fernandes <98667548+pamialvs@users.noreply.github.com>
2025-02-21 18:53:27 -0300

    Add files via upload

---
+++ q9.py
@@ -0,0 +1,4 @@
+# Sua tarefa é criar uma função que receba um número e retorne o dobro do seu valor.
+numero = int(input("Digite um número para saber o dobro: "))
+dobro = numero * 2
+print("O dobro de", numero, "é", dobro)
